5d3449e8bdd414d368526fdb2084ddc8412b6c18
Jason Yu <ycm.jason@gmail.com>
2016-03-04 02:06:11 +0000

    fixed generatec for elise

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Cryptography Game
 =================
 
+Rehosted at: [http://elise.ycmjason.com/spc-cryptogame/](http://elise.ycmjason.com/spc-cryptogame/)
+
 I built this in 2011 for the 160th Anniversary Open Day of St. Paul's College.
--- generatec.php
+++ generatec.php
@@ -9,5 +9,5 @@ $file = fopen("passage.php","a");
 fwrite($file, $content);
 fclose($file);
 echo "Here\'s your Challenge for your friends. please copy the link and tell your friends.<br />
-<a href=\"http://encryption.votelamp.com/?p=".$id."\">http://encryption.votelamp.com/?p=".$id."</a>";
+<a href=\"http://elise.ycmjason.com/?p=".$id."\">http://elise.ycmjason.com/?p=".$id."</a>";
 ?>
